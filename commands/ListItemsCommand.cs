17fd1f67c08c2adbb01bca023470eda4862322c5
Patrick Wilbur <pjwilbur99@gmail.com>
2023-10-12 23:17:40 -0400

    fix/update list-items method to filter on status and timeline

--- commands/ListItemsCommand.cs
+++ commands/ListItemsCommand.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
 using Productivity;
 using Spectre.Console;
@@ -9,7 +10,7 @@ public class ListItemsCommand : Command<ListItemsCommand.Settings>
     struct listResult
     {
         public Item Item;
-        public Productivity.Status Status;
+        public ItemStatusUpdate? Update;
     }
 
     public ListItemsCommand(ItemStore itemStore)
@@ -18,20 +19,46 @@ public class ListItemsCommand : Command<ListItemsCommand.Settings>
     }
 
     public class Settings : CommandSettings
-    { }
+    {        
+        [CommandOption("-s|--status")]
+        [Description("List items only with the provided status")]
+        public Productivity.Status? Status { get; set; }
+
+        [CommandOption("-t|--timeline")]
+        [Description("Provide a timeline to list items that have the provided status on the provided timeline. If no status provided, defaults to Done")]
+        public Productivity.Schedule? Timeline { get; set; }
+    }
 
     public override int Execute([NotNull] CommandContext context, [NotNull] Settings settings)
     {
-
-
-        var items = _itemStore.Items.Select(i => new listResult
+        // todo -- update function so status is determined by schedule 
+        // so we want a list of items that match the specified status
+        // currently we just grab the latest update
+        // i think we check to see if that update is on current day/week/month
+        var itemQuery = _itemStore.Items.Select(i => new listResult
         {
             Item = i,
-            Status = i.Updates
-                .OrderByDescending(u => u.UpdateTimestamp)
-                .Select(p => p.status)
-                .FirstOrDefault()                
-        });
+            Update = i.Updates
+                    .OrderByDescending(u => u.UpdateTimestamp)
+                    .First()
+        });        
+
+        if (settings.Status != null && settings.Status != Productivity.Status.Incomplete)
+        {
+            itemQuery = itemQuery.Where(lr => lr.Update != null && lr.Update.status == settings.Status);
+        }
+
+        if (settings.Status == null || settings.Status == Productivity.Status.Incomplete)
+        {
+            itemQuery = itemQuery.Where(lr => lr.Update != null || (lr.Update != null && lr.Update.status == Productivity.Status.Incomplete));
+        }
+
+        if (settings.Timeline != null)
+        {
+            itemQuery = itemQuery.Where(lr => lr.Item.schedule == settings.Timeline);
+        }
+
+        var items = itemQuery.ToList();
 
         var table = new Table();
 
@@ -40,24 +67,26 @@ public class ListItemsCommand : Command<ListItemsCommand.Settings>
         table.AddColumn("Title");
         table.AddColumn("Priority");
         table.AddColumn("Status");
+        table.AddColumn("Schedule");
 
         // Add some rows
         foreach (listResult lr in items)
         {
             var item = lr.Item;
+            var status = lr.Update?.status;
             var shortId = item.id.ToString().Split('-')[0];
 
             var statusColor = "blue";
-            // if (item.status == Productivity.Status.InProgress) statusColor = "green";
-            // if (item.status == Productivity.Status.Aborted) statusColor = "red";
-            // if (item.status == Productivity.Status.Done) statusColor = "grey";
+            if (status == Productivity.Status.InProgress) statusColor = "green";
+            if (status == Productivity.Status.Aborted) statusColor = "red";
+            if (status == Productivity.Status.Done) statusColor = "grey";
 
             var priorityColor = "blue";
             if (item.priority == Priority.ImportantForHealth) priorityColor = "red";
             if (item.priority == Priority.Maintenance) priorityColor = "yellow";
             if (item.priority == Priority.WouldBeCool) priorityColor = "purple";
 
-            table.AddRow($"[grey]{shortId}[/]", $"[blue]{item.title}[/]", $"[{priorityColor}]{item.priority}[/]", $"[{statusColor}]{lr.Status}[/]");
+            table.AddRow($"[grey]{shortId}[/]", $"[blue]{item.title}[/]", $"[{priorityColor}]{item.priority}[/]", $"[{statusColor}]{status}[/]", $"{item.schedule}");
         }
 
         // Render the table to the console
